f53c58b67173d12d3665b3caf8e4e6aeaf9f9939
Tapswi Yadav <sytapswi@gmail.com>
2025-11-12 16:56:00 +0530

    deploy

--- src/app/api.js
+++ src/app/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "https://hindipathshalabackend.onrender.com/";
+  import.meta.env.VITE_API_URL || "https://hindipathshalabackend.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
